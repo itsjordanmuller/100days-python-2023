84c0bd5c7d5bd21b940f347b8059e6979027221c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-06 20:39:27 -0800

    Comment out type tests, add calculation for BMI, print BMI as integer to not lose detail of float variable

--- Day2/Exercises/cr_bmi.py
+++ Day2/Exercises/cr_bmi.py
@@ -1,4 +1,6 @@
 height = float(input("enter your height: "))
-print (type(height))
+# print (type(height))
 weight = float(input("enter your weight: "))
-print (type(weight))
+# print (type(weight))
+bmi = (weight)/(height ** 2)
+print(int(bmi))
