4b2ebc43eb3342e32a85752e88678902886cea36
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-06 20:20:00 -0800

    Create two variables, digit_one and digit_two that store the first and second character of string as ints, print sum

--- Day2/Exercises/cr_data-types.py
+++ Day2/Exercises/cr_data-types.py
@@ -1,2 +1,6 @@
 two_digit_num = input("Type a two digit number: ")
 # print(type(two_digit_num))
+digit_one = int(two_digit_num[0])
+digit_two = int(two_digit_num[1])
+
+print(digit_one + digit_two)
