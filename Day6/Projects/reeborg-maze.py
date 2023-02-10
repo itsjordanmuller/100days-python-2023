908a9fe0822b4b3fbfda4241ea7927a3c1e3a4ad
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-10 12:52:57 -0800

    Updated Reeborg's maze while loop to allow him to get out of loops

--- Day6/Projects/reeborg-maze.py
+++ Day6/Projects/reeborg-maze.py
@@ -14,3 +14,22 @@
 #         move()
 #     else:
 #         turn_left()
+
+# Challenge Test Maze 1 2 & 3
+# Reeborg Gets Stuck Sometimes, Use the Below Code to Fix:
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear() and front_is_clear():
+#         turn_left()
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
