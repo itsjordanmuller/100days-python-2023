a78eec8a5f89f390352f65ba7181d9ceddbb6456
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-11 18:04:24 -0800

    Removed commented code and todos from previous tasks

--- Day7/Projects/parachute-man.py
+++ Day7/Projects/parachute-man.py
@@ -333,22 +333,7 @@ chosen_word = random.choice(word_list)
 display = ["_"] * len(chosen_word)
 print(display)
 
-# while "_" in display:
-#     guess = input("Choose a single letter: ").lower()
-#     for i, letter in enumerate(chosen_word):
-#         if letter == guess:
-#             display[i] = letter
-#     print(display)
-# print("Game over, you won!")
-
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
-
 lives = 6
-
-#TODO-2: - If guess is not a letter in the chosen_word,
-#Then reduce 'lives' by 1. 
-#If lives goes down to 0 then the game should stop and it should print "You lose."
 
 while "_" in display and lives > 0:
     print(stages[6-lives])
@@ -365,5 +350,3 @@ if lives == 0:
     print(result[0])
 else:
     print(result[1])
-
-#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
