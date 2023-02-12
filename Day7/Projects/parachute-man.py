39ad7516292bf99cf30b28512e8e49e3dcfcc88c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-11 16:50:55 -0800

    Instead of checking if it is in the array, check and print each letter in the word

--- Day7/Projects/parachute-man.py
+++ Day7/Projects/parachute-man.py
@@ -9,11 +9,16 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-#chosen_word = word_list[random.randint(0, (len(word_list)-1))]
+# chosen_word = word_list[random.randint(0, (len(word_list)-1))]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 guess = input("Choose a single letter: ").lower()
-if guess in chosen_word:
-    print("That is one of the letters.")
-else:
-    print("That is not one of the letters.")
+# if guess in chosen_word:
+#     print("That is one of the letters.")
+# else:
+#     print("That is not one of the letters.")
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
