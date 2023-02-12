37ca8825c5576e907fb0411cd2333cfe15f1f95e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-11 17:14:50 -0800

    Incorporated elements of challenge 2 & 3 into a single challenge 2

--- Day7/Exercises/parachute-man-1.py
+++ Day7/Exercises/parachute-man-1.py
@@ -1,22 +1,24 @@
-#Step 1 
+# Step 1 
 
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
 # chosen_word = word_list[random.randint(0, (len(word_list)-1))]
 chosen_word = random.choice(word_list)
 print(chosen_word)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
 guess = input("Choose a single letter: ").lower()
 # if guess in chosen_word:
 #     print("That is one of the letters.")
 # else:
 #     print("That is not one of the letters.")
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
 for letter in chosen_word:
     if letter == guess:
         print(guess)
