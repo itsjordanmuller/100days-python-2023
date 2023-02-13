17c9f2134a718cc7559c9bb57d54f470d24d22cb
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-12 20:55:16 -0800

    Add ability to input user location

--- Day8/Exercises/greet-function.py
+++ Day8/Exercises/greet-function.py
@@ -1,8 +1,9 @@
 name = input("What is the name of the person you are greeting? ")
+location = input("Where do they live? ")
 
-def greet(name):
+def greet(name, location):
     print(f"Hello {name}!")
-    print(f"How are you doing today {name}?")
+    print(f"What is it like in {location}?")
     print(f"Isn't it lovely out today, {name}?")
 
-greet(name)
+greet(name, location)
