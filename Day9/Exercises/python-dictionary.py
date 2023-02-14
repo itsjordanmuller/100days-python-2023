b35aa6c3670b7484a3be93ca3b6c8090c0f1b87e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-13 17:18:16 -0800

    Add exercises for day 1 start dictionary practice

--- Day9/Exercises/python-dictionary.py
+++ Day9/Exercises/python-dictionary.py
@@ -1,8 +1,29 @@
 programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again."
+    "Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."
 }
 
 print(programming_dictionary["Bug"])
 print(programming_dictionary["Function"])
+
+print(programming_dictionary)
+
+# Assign New Term to New Key in Dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
 print(programming_dictionary["Loop"])
+print(programming_dictionary)
+
+# Create an empty dictionary
+empty_dictionary = {}
+
+# Wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+# Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
