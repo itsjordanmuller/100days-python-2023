0154c2e0696cd285c51db42a1278f46715d26589
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-13 17:41:56 -0800

    Add exercises for practicing nesting dictionaries in lists and vice versa

---
+++ Day9/Exercises/nesting-dictionaries.py
@@ -0,0 +1,27 @@
+# Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+# Nesting a List in a Dictionary
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"],"total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"],"total_visits": 12}
+}
+
+# Nesting a Dictionary in a List
+
+travel_log = {
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany", 
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 12
+    },
+}
