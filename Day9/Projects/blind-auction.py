e005dfa264895ed676eacb387dad944a58f116ad
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-13 18:04:20 -0800

    Solution for Blind Auction program, with temporary placeholder art for the gavel

---
+++ Day9/Projects/blind-auction.py
@@ -0,0 +1,22 @@
+from art import logo
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+print(logo)
+print("Welcome to the blind auction program.")
+
+bidders = []
+bidding = "y"
+
+while bidding == "y":
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    bidders.append({"name": name, "bid": bid})
+    bidding = input("Are there any other bidders? Type 'y' or 'n': ")
+    if bidding == "y":
+        clear()
+
+winner = max(bidders, key=lambda x: x['bid'])
+print("The winner is", winner["name"], "with a bid of $" + str(winner["bid"]))
