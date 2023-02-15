e63d00aefc3c3c1845d191bbf1a9cee08afabb0f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-14 21:04:48 -0800

    Create an example function for warmup, then create format name function and print

---
+++ Day10/Exercises/functions-with-outputs.py
@@ -0,0 +1,12 @@
+def my_function():
+    result = 3 * 2
+    return result
+
+my_function()
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    print(f"{formatted_f_name} {formatted_l_name}")
+
+format_name("JoRdAn", "MULLER")
