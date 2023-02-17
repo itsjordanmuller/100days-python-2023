be1f6b96ec8bfe8340720cfa2e87e0a10b869638
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-16 20:27:52 -0800

    Remove extra logo and rules

--- Day011/Exercises/building-blackjack.py
+++ Day011/Exercises/building-blackjack.py
@@ -1,8 +1,6 @@
 import random
 
 from art import logo
-print(logo)
-print("------------------  HOUSE RULES  ------------------\n1. The deck is unlimited in size.\n2. There are no jokers.\n3. The Jack/Queen/King all count as 10.\n4. The the Ace can count as 11 or 1.\n5. Cards are not removed from the deck as they are drawn.\n6. The computer is the dealer.\n---------------------  START  ---------------------")
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
