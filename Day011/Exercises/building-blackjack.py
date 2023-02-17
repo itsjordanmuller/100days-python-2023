7667cd75023a2ce014c5eac22a88fa5725001a88
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-16 20:36:22 -0800

    Add function to compare scores and determine a winner

--- Day011/Exercises/building-blackjack.py
+++ Day011/Exercises/building-blackjack.py
@@ -15,6 +15,22 @@ def calculate_score(cards):
         score = sum(cards)
     return score
 
+def compare(player_score, computer_score):
+    if player_score > 21:
+        return "You went over. You lose."
+    elif computer_score > 21:
+        return "Computer went over. You win!"
+    elif player_score == computer_score:
+        return "It's a draw."
+    elif computer_score == 0:
+        return "Computer has a blackjack. You lose."
+    elif player_score == 0:
+        return "You have a blackjack! You win."
+    elif player_score > computer_score:
+        return "You win!"
+    else:
+        return "You lose."
+
 def play_game():
     print(logo)
     print("------------------  HOUSE RULES  ------------------\n1. The deck is unlimited in size.\n2. There are no jokers.\n3. The Jack/Queen/King all count as 10.\n4. The the Ace can count as 11 or 1.\n5. Cards are not removed from the deck as they are drawn.\n6. The computer is the dealer.\n---------------------  START  ---------------------")
