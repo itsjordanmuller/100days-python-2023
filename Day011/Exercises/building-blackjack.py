157d778f59d56cfe0c6b07fa19ac321a748b8018
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-16 20:34:27 -0800

    Definte function to calculate and return score

--- Day011/Exercises/building-blackjack.py
+++ Day011/Exercises/building-blackjack.py
@@ -7,6 +7,14 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 def deal_card():
     return random.choice(cards)
 
+def calculate_score(cards):
+    score = sum(cards)
+    if 11 in cards and score > 21:
+        cards.remove(11)
+        cards.append(1)
+        score = sum(cards)
+    return score
+
 def play_game():
     print(logo)
     print("------------------  HOUSE RULES  ------------------\n1. The deck is unlimited in size.\n2. There are no jokers.\n3. The Jack/Queen/King all count as 10.\n4. The the Ace can count as 11 or 1.\n5. Cards are not removed from the deck as they are drawn.\n6. The computer is the dealer.\n---------------------  START  ---------------------")
@@ -25,6 +33,7 @@ def play_game():
         else:
             game_over = True
 
-play_again = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y"
+play_again = "y"
 while play_again == "y":
     play_game()
+    play_again = input("Do you want to play again? Type 'y' or 'n': ")
