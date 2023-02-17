ae391aec1ca60bd09c5d478e02de80f2cb0edfa3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-16 20:21:51 -0800

    Prompt user to play another game, as well as set game_over to False to start

--- Day011/Exercises/building-blackjack.py
+++ Day011/Exercises/building-blackjack.py
@@ -10,3 +10,16 @@ def deal_card():
     print(random.choice(cards))
 
 deal_card()
+
+def play_game():
+    game_over = False
+
+    while not game_over:
+        play_game()
+
+        print(logo)
+        print("------------------  HOUSE RULES  ------------------\n1. The deck is unlimited in size.\n2. There are no jokers.\n3. The Jack/Queen/King all count as 10.\n4. The the Ace can count as 11 or 1.\n5. Cards are not removed from the deck as they are drawn.\n6. The computer is the dealer.\n---------------------  START  ---------------------")
+
+play_again = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y"
+while play_again == "y":
+    play_game()
