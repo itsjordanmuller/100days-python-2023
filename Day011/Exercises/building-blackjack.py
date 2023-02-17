9e2ce8577262b183d5a5435a4a891d7241ed143d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-16 20:26:29 -0800

    Moved rules and logo to play_game, added a blank list for player and computer cards, deal initial cards

--- Day011/Exercises/building-blackjack.py
+++ Day011/Exercises/building-blackjack.py
@@ -7,18 +7,21 @@ print("------------------  HOUSE RULES  ------------------\n1. The deck is unlim
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
-    print(random.choice(cards))
-
-deal_card()
+    return random.choice(cards)
 
 def play_game():
+    print(logo)
+    print("------------------  HOUSE RULES  ------------------\n1. The deck is unlimited in size.\n2. There are no jokers.\n3. The Jack/Queen/King all count as 10.\n4. The the Ace can count as 11 or 1.\n5. Cards are not removed from the deck as they are drawn.\n6. The computer is the dealer.\n---------------------  START  ---------------------")
+    player_cards = []
+    computer_cards = []
     game_over = False
 
-    while not game_over:
-        play_game()
+    for i in range(2):
+        player_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
-        print(logo)
-        print("------------------  HOUSE RULES  ------------------\n1. The deck is unlimited in size.\n2. There are no jokers.\n3. The Jack/Queen/King all count as 10.\n4. The the Ace can count as 11 or 1.\n5. Cards are not removed from the deck as they are drawn.\n6. The computer is the dealer.\n---------------------  START  ---------------------")
+    # while not game_over:
+    #     #
 
 play_again = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y"
 while play_again == "y":
