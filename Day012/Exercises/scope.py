67f90a06e515177548914fc88ecc588b7f096c41
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-17 22:45:58 -0800

    Add global scope practice

--- Day012/Exercises/scope.py
+++ Day012/Exercises/scope.py
@@ -11,8 +11,18 @@ print(f"enemies outside function: {enemies}")
 
 # Local Scope
 
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+
+# drink_potion()
+
+
+# Global Scope
+player_health = 10
+
 def drink_potion():
     potion_strength = 2
-    print(potion_strength)
+    print(player_health)
 
 drink_potion()
